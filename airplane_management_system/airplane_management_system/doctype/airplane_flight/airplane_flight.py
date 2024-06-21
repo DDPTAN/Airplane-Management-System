b3ee4beeb4f9f8eadcc8b136a902ee9147865935
alana070700 <alana@tandigital.id>
2024-06-21 10:14:24 +0700

    feat: add Airplane Flight

--- airplane_management_system/airplane_management_system/doctype/airplane_flight/airplane_flight.py
+++ airplane_management_system/airplane_management_system/doctype/airplane_flight/airplane_flight.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2024, BroCode and contributors
 # For license information, please see license.txt
 
-import frappe
+# import frappe
 from frappe.model.document import Document
 
 
-class Airline(Document):
+class AirplaneFlight(Document):
 	pass
