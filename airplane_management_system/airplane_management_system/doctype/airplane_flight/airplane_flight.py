24b06cc1f296d2e60d21756a75c4b3e1c155c7cc
William Kristiawan <william@tandigital.id>
2024-06-22 15:12:35 +0700

    feat: Set Status to Completed on Submission of Airplane Flight DocType and Status Not Equal to Boarded on Airplane Ticket

--- airplane_management_system/airplane_management_system/doctype/airplane_flight/airplane_flight.py
+++ airplane_management_system/airplane_management_system/doctype/airplane_flight/airplane_flight.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2024, BroCode and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class AirplaneFlight(Document):
-	pass
+	def before_submit(self):
+		self.status = "Completed"
+	
+	@frappe.whitelist()
+	def set_flight_status(self):
+		flight = frappe.get_doc("Airplane Flight",)
+		flight.status = "Completed"
+		flight.save()
+
