4a3def3cfad040f60a1cc52a1c9f790670b75f0e
William Kristiawan <william@tandigital.id>
2024-06-22 14:17:17 +0700

    feat: add seat field and add script to assign seat

--- airplane_management_system/airplane_management_system/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_management_system/airplane_management_system/doctype/airplane_ticket/airplane_ticket.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2024, BroCode and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+import random
+import string
 from frappe.model.document import Document
 
 
 class AirplaneTicket(Document):
-	pass
+	def generate_seat(self):
+		seat_number = random.randint(1,100)
+		seat_letter = random.choice(string.ascii_uppercase[:5])
+		self.seat = f"{seat_number}{seat_letter}"
