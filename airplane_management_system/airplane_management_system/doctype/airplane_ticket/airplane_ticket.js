2cca03ee399383653a5d1032a6e87eb8b979b264
William Kristiawan <william@tandigital.id>
2024-06-26 11:30:10 +0700

    feat: Refactoring Airplane Ticket , Airplane, Airline, Airplane Flight

--- airplane_management_system/airplane_management_system/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_management_system/airplane_management_system/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,26 @@
-// Copyright (c) 2024, BroCode and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Airplane Ticket", {
+    flight_price(frm)
+    {
+        let total_amount =frm.doc.flight_price
+        for(let item of frm.doc.add_ons)
+        {
+            total_amount+= item.amount
+        }
+        frm.set_value("total_amount" ,total_amount) 
+    }
+});
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
 
-// 	},
-// });
+frappe.ui.form.on("Airplane Ticket Add-on Item", {
+	 
+
+    amount(frm,cdt,cdn)
+    {
+        let total_amount =frm.doc.flight_price
+        for(let item of frm.doc.add_ons)
+        {
+            total_amount+= item.amount
+        }
+        frm.set_value("total_amount" ,total_amount) 
+    },
+});
--- airplane_management_system/airplane_management_system/doctype/booking/test_booking.py
+++ airplane_management_system/airplane_management_system/doctype/booking/test_booking.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestFlightPassenger(FrappeTestCase):
+class TestBooking(FrappeTestCase):
 	pass
