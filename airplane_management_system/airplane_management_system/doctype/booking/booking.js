6950ca68bb4872067ec95a713b90fdae81fe73eb
William Kristiawan <william@tandigital.id>
2024-06-27 00:05:29 +0700

    feat: Update Booking Process

--- airplane_management_system/airplane_management_system/doctype/booking/booking.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, BroCode and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Booking", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- airplane_management_system/airplane_management_system/doctype/flight_passenger/flight_passenger.py
+++ airplane_management_system/airplane_management_system/doctype/flight_passenger/flight_passenger.py
@@ -6,4 +6,9 @@ from frappe.model.document import Document
 
 
 class FlightPassenger(Document):
-	pass
+    def before_save(self):    
+        # Passenger Name Validation
+        if self.last_name:
+            self.full_name = self.first_name + ' ' + self.last_name
+        else:
+            self.full_name = self.first_name
