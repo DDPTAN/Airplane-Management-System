61c372ea37ab0229f95a61aba2c9280f486b0c51
William Kristiawan <william@tandigital.id>
2024-06-28 10:20:17 +0700

    fix: fixing generate flight passenger full name

--- airplane_management_system/airplane_management_system/doctype/flight_passenger/flight_passenger.py
+++ airplane_management_system/airplane_management_system/doctype/flight_passenger/flight_passenger.py
@@ -1,16 +1,16 @@
 # Copyright (c) 2024, BroCode and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class FlightPassenger(Document):
-    def before_save(self):    
-        # Passenger Name Validation
+    def generate_full_name(self):
         if self.last_name:
-            self.full_name = self.first_name + ' ' + self.last_name
+            self.full_name = f"{self.first_name} {self.last_name}".strip()
         else:
-            self.full_name = self.first_name
+            self.full_name = f"{self.first_name}"
 
-        self.booking_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    def before_save(self):
+        self.generate_full_name()
